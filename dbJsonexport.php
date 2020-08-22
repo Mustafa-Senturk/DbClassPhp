f6817672ccd0bf0ef3736257d4c23caad5b90d66
mustafa şentürk <31421506+Mustafa-Senturk@users.noreply.github.com>
2020-08-22 20:09:12 +0300

    Update dbJsonexport.php

--- dbJsonexport.php
+++ dbJsonexport.php
@@ -2,7 +2,17 @@
 require_once('dbClass.php');
 $tablo 	= $_GET['t'];
 $id 	= @$_GET['id'];
-$cikti = $dbSorgula->co($tablo);
-header("Content-type: application/json; charset=utf-8");
-print_r(json_encode($cikti));
+$token  = @$_GET['token'];
+$key = 	'1q2w3e';
+if($key == $token && isset($_GET['token']) ){
+	$cikti = $dbSorgula->co($tablo);
+	header('Access-Control-Allow-Origin: *');
+	header("Content-type: application/json; charset=utf-8");
+	echo json_encode($cikti);
+}else{
+	header('HTTP/1.1 400 Not Found');
+	echo 'lütfen Parametrelerinizi kontrol edin !...';
+}
+
+
 ?>
